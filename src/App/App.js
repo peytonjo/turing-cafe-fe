d807cd7adcb466f5be697260c21b32d9002d6355
Peyton Jones <peytonj.dev@gmail.com>
2020-12-16 09:33:47 -0700

    Add Card element components to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from '../Card/Card.js'
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <Card /> 
         </div>
       </div>
     )
